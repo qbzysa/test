0de1dd0ad6ce9b41cb54b3615d22eba0bb4eba59
qbzysa <2541183419@qq.com>
2019-03-19 16:07:18 +0800

    Update send_email.py

--- auto_send_message/send_email.py
+++ auto_send_message/send_email.py
@@ -10,47 +10,15 @@ from email.mime.image import MIMEImage
 
 def email_test():
     smtp = smtplib.SMTP()
-    smtp.connect('mail.pansafe.com', 25)
-    smtp.login('qiubenzhao@pansafe.com', '8yD2WwRa3A')
-    # 邮件内容
-    mail_subject = "python发送邮件测试"         # 邮件的标题
-    mail_context = "这是邮件内容"
-
-    msg = MIMEMultipart()
-    msg["From"] = 'qiubenzhao@pansafe.com'  # 发件人
-    msg["To"] = '2541183419@qq.com'  # 收件人
-    msg["Subject"] = mail_subject   # 邮件标题
-    # 邮件正文
-    msg.attach(MIMEText(mail_context, 'plain', 'utf-8'))
-    # 图片附件
-    # 不同的目录下要写全文件路径
-    with open('E:\\autotest\\result\\image\\2018-12-26\\2018-12-26-10_22_11.png', 'rb') as picAtt:
-        msgImg = MIMEImage(picAtt.read())
-        msgImg.add_header('Content-Disposition', 'attachment', filename="test.png")
-        msg.attach(msgImg)
-
-    # 构造附件
-    att = MIMEText(open('E:\\autotest\\result\\SoftTestReport_2018-12-26-10_20_20.html', "rb").read(), "base64", "utf-8")
-    att["Content-Type"] = "application/octet-stream"
-    # 附件名称
-    att.add_header("Content-Disposition", "attachment", filename="test.html")
-    msg.attach(att)
-
-    smtp.sendmail('qiubenzhao@pansafe.com', '2541183419@qq.com', msg.as_string())
-    smtp.quit()
-
-
-def email_test2():
-    smtp = smtplib.SMTP()
     smtp.connect('smtp.qq.com', 25)
-    smtp.login('2541183419@qq.com', 'bjohabvbvccdebhi')
+    smtp.login('*****', '*****')
     # 邮件内容
     mail_subject = "python发送邮件测试"  # 邮件的标题
     mail_context = "这是邮件内容"
 
     msg = MIMEMultipart()
-    msg["From"] = '2541183419@qq.com'  # 发件人
-    msg["To"] = 'qiubenzhao@pansafe.com'  # 收件人
+    msg["From"] = '*****'  # 发件人
+    msg["To"] = '*****'  # 收件人
     msg["Subject"] = mail_subject  # 邮件标题
     # 邮件正文
     msg.attach(MIMEText(mail_context, 'plain', 'utf-8'))
@@ -76,4 +44,4 @@ def email_test2():
 
 
 if __name__ == "__main__":
-    email_test2()
+    email_test()
