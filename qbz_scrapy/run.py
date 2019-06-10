6d24d88dc1c9bb5b0ab53822d40dc39afcf59a29
qbzysa <2541183419@qq.com>
2019-06-10 10:52:16 +0800

    Update run.py

--- qbz_scrapy/run.py
+++ qbz_scrapy/run.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     # cmdline.execute("scrapy crawl github".split())
-    #cmdline.execute("scrapy crawl maoyan".split())
+    # cmdline.execute("scrapy crawl maoyan".split())
     # cmdline.execute("scrapy crawl kuaidi100".split())
+    # cmdline.execute("scrapy crawl fam".split())
     cmdline.execute("scrapy crawl carlogo".split())
